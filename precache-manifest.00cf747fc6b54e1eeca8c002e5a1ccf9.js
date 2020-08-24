bb73c816f7bcca7a002b7e225aa6a00d5587775d
sts2003 <44109148+sts2003@users.noreply.github.com>
2020-08-25 01:58:27 +0900

    Updates

---
+++ precache-manifest.00cf747fc6b54e1eeca8c002e5a1ccf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ff8adbc186e4b19b56470db8ec72278",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "6fcae01ed311115cd532",
+    "url": "/movie_app/static/css/main.9728e8eb.chunk.css"
+  },
+  {
+    "revision": "48d04c1484dc82f93f2a",
+    "url": "/movie_app/static/js/2.77275e5f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app/static/js/2.77275e5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fcae01ed311115cd532",
+    "url": "/movie_app/static/js/main.ed3f1db7.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
